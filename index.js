8a37d324ef0f1b88d0651959dbd94665c7682605
Premanshu Patel <pmpatel@udel.edu>
2021-09-08 14:07:42 -0400

    added some changes to index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$( document ).ready(function() {
-console.log( "ready!" );
+$( "p" ).on( "myCustomEvent", function( event, fname, lname ) {
+    $( this ).text( myName + ", hi there!" );
+    $( "span" )
+      .stop()
+      .css( "opacity", 1 )
+      .text( fname + " " + lname )
+      .fadeIn( 30 )
+      .fadeOut( 1000 );
+});
+
+$( "button" ).click(function() {
+    $( "p" ).trigger("myCustomEvent", "fname", "lname");
 });
